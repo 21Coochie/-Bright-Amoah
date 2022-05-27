1a9a407d6936bacc98adc69ccfebd66bbf1b408d
21Coochie <brghtmalone@gmail.com>
2022-05-27 16:37:17 +0000

    Initial commit of python code

---
+++ main.py
@@ -0,0 +1,7 @@
+# A Program That Asks For Your Name And Displays It
+
+print("A Program That Asks For Your Name And Displays It")
+
+name = input(" Please enter your name:")
+
+print("Your name is " + name)
